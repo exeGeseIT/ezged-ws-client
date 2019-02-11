425865f29a79027e97e234f9e187f68b8de63e9f
jc.glombard <jean-claude.glombard@amtrust.fr>
2019-02-11 15:14:37 +0100

    Core::JOBSTATUS_xxx

--- src/Component/CoreBase.php
+++ src/Component/CoreBase.php
@@ -38,6 +38,43 @@ abstract class CoreBase
 
     const ERRORCODE_OK = 0;
 
+
+    
+    /**
+     * Le travail est prêt et en attente d'être traité par le serveur de travaux
+     */
+    const JOBSTATUS_PENDING = 0;
+
+    /**
+     * Le travail est en file, i.e sélectionné par le serveur de travaux pour s'exécuter
+     */
+    const JOBSTATUS_QUEUED = 1;
+
+    /**
+     * Le travail est terminé. Il sera purgé automatiquement
+     */
+    const JOBSTATUS_ENDED_PURGEABLE = 2;
+
+    /**
+     * Le travail est terminé. Il ne sera pas purgé automatiquement
+     */
+    const JOBSTATUS_ENDED_NOT_PURGEABLE = 3;
+
+    /**
+     * Le travail est en erreur. Des tentatives d'exécution seront faites périodiquement.
+     * (par défaut toutes les 3 mins)
+     */
+    const JOBSTATUS_ERROR = 4;
+
+    /**
+     * Le travail a rencontré une erreur critique.
+     *   => il est désactivé ==>> pas de ré-exécution périodique.
+     * Ce status est également utilisé pour mettre un travail en attente de l'indexation complète de son document (Centre d'indexation)
+     */
+    const JOBSTATUS_CRITICAL = 5;
+
+
+
     /**
      * Ouvrir une session (Authent)
      */
