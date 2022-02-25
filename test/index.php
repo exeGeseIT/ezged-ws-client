d26f638baacf03c79e9103287178adeca822e4b6
jc.glombard <jc.glombard@gmail.com>
2022-02-25 17:05:39 +0100

    [REWORK (WiP)]: cleanup (remove all v1.x stuff)

--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
 
-use ExeGeseIT\EzGEDWsClient\Core\EzGEDResponseInterface;
 use ExeGeseIT\EzGEDWsClient\EzGEDClient;
+use ExeGeseIT\EzGEDWsClient\EzGEDHelper;
 use Symfony\Component\HttpClient\HttpClient;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,19 +16,43 @@ $httpclient = HttpClient::create([
     ],
 ]);
 
-$ezWS = new EzGEDClient($httpclient, $config['user'], $config['pwd']);
+$ezWS = new EzGEDClient($httpclient, $config['user'], $config['pwd'], $config['api']);
 
-/* */
-$reponse = $ezWS->connect(true);
+/** /
+$response = $ezWS->connect(true);
 dump([
-    'message' => $reponse->getMessage(),
-    'connect' => $reponse->getHttpHeaders(),
+    'message' => $response->getMessage(),
+    'connect' => $response->getHttpHeaders(),
     '$ezWS' => $ezWS,
 ]);
 
-/* */
-$reponse = $ezWS->getPerimeter();
-dump(['getPerimeter' => $reponse]);
+/** /
+$response = $ezWS->logout();
+dump([
+    '$ezWS' => $ezWS,
+]);
+
+/** /
+$response = $ezWS->getPerimeter();
+dump(['getPerimeter' => $response]);
+
+/**/
+$idquery = 3;
+$response = $ezWS->search($idquery, null, 5, EzGEDHelper::likeSearchFilter('DOCUMENTRECRUTEMENT_APPLICANT', 'CHA'));
+dump(['search' => $response]);
+
+/**/
+$idrecord = 249387;
+$recordTable = 'DOCUMENTRECRUTEMENT';
+$response = $ezWS->getRecordPages($idrecord, $recordTable);
+dump(['page' => $response]);
+
+/** /
+$fsfileid = 256145;
+$fsfileripe = '9258344193e53eb8ef8eb8a5ad25a806492db2b6';
+$response = $ezWS->downloadFile($fsfileid, $fsfileripe, './download/');
+dump(['download' => $response]);
+
 
 /*
 */
