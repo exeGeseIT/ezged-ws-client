112a794c31b51a3c2de6dc664a802c577480ec79
jc.glombard <jc.glombard@gmail.com>
2022-04-29 20:48:53 +0200

    cleanup

--- src/Core/EzGEDResponseAbstract.php
+++ src/Core/EzGEDResponseAbstract.php
@@ -37,16 +37,7 @@ abstract class EzGEDResponseAbstract implements EzGEDResponseInterface
     }
     
     /**
-     * Gets the HTTP headers of the http response.
-     *
-     * @param bool $throw Whether an exception should be thrown on 3/4/5xx status codes
-     *
-     * @return string[][] The headers of the response keyed by header names in lowercase
-     *
-     * @throws TransportExceptionInterface   When a network error occurs
-     * @throws RedirectionExceptionInterface On a 3xx when $throw is true and the "max_redirects" option has been reached
-     * @throws ClientExceptionInterface      On a 4xx when $throw is true
-     * @throws ServerExceptionInterface      On a 5xx when $throw is true
+     * {@inheritDoc}
      */
     public function getHttpHeaders(bool $throw = true): array
     {
@@ -54,7 +45,7 @@ abstract class EzGEDResponseAbstract implements EzGEDResponseInterface
     }
 
     /**
-     * @throws TransportExceptionInterface when a network error occurs
+     * {@inheritDoc}
      */
     public function getHttpStatusCode(): int
     {
@@ -62,31 +53,7 @@ abstract class EzGEDResponseAbstract implements EzGEDResponseInterface
     }
     
     /**
-     * Returns info coming from the transport layer.
-     *
-     * This method SHOULD NOT throw any ExceptionInterface and SHOULD be non-blocking.
-     * The returned info is "live": it can be empty and can change from one call to
-     * another, as the request/response progresses.
-     *
-     * The following info MUST be returned:
-     *  - canceled (bool) - true if the response was canceled using ResponseInterface::cancel(), false otherwise
-     *  - error (string|null) - the error message when the transfer was aborted, null otherwise
-     *  - http_code (int) - the last response code or 0 when it is not known yet
-     *  - http_method (string) - the HTTP verb of the last request
-     *  - redirect_count (int) - the number of redirects followed while executing the request
-     *  - redirect_url (string|null) - the resolved location of redirect responses, null otherwise
-     *  - response_headers (array) - an array modelled after the special $http_response_header variable
-     *  - start_time (float) - the time when the request was sent or 0.0 when it's pending
-     *  - url (string) - the last effective URL of the request
-     *  - user_data (mixed) - the value of the "user_data" request option, null if not set
-     *
-     * When the "capture_peer_cert_chain" option is true, the "peer_certificate_chain"
-     * attribute SHOULD list the peer certificates as an array of OpenSSL X.509 resources.
-     *
-     * Other info SHOULD be named after curl_getinfo()'s associative return value.
-     *
-     * @return mixed An array of all available info, or one of them when $type is
-     *               provided, or null when an unsupported type is requested
+     * {@inheritDoc}
      */
     public function getHttpInfo(string $type = null): array
     {
@@ -94,23 +61,41 @@ abstract class EzGEDResponseAbstract implements EzGEDResponseInterface
     }
     
     
+    /**
+     * {@inheritDoc}
+     */
+    public function getErrorNumber(): int
+    {
+        return $this->error;
+    }
     
+    /**
+     * {@inheritDoc}
+     */
     public function isSucceed(): bool
     {
         return $this->error === 0;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     public function getMessage(): ?string
     {
         return $this->message;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     public function getCount(): int
     {
         return $this->count;
     }
     
-    
+    /**
+     * {@inheritDoc}
+     */
     public function getContent(): ?ParameterBagInterface
     {
         return $this->content;
--- src/Core/EzGEDResponseInterface.php
+++ src/Core/EzGEDResponseInterface.php
@@ -36,9 +36,46 @@ interface EzGEDResponseInterface
     public function getHttpHeaders(bool $throw = true): array;
     
     /**
+     * Returns info coming from the transport layer.
+     *
+     * This method SHOULD NOT throw any ExceptionInterface and SHOULD be non-blocking.
+     * The returned info is "live": it can be empty and can change from one call to
+     * another, as the request/response progresses.
+     *
+     * The following info MUST be returned:
+     *  - canceled (bool) - true if the response was canceled using ResponseInterface::cancel(), false otherwise
+     *  - error (string|null) - the error message when the transfer was aborted, null otherwise
+     *  - http_code (int) - the last response code or 0 when it is not known yet
+     *  - http_method (string) - the HTTP verb of the last request
+     *  - redirect_count (int) - the number of redirects followed while executing the request
+     *  - redirect_url (string|null) - the resolved location of redirect responses, null otherwise
+     *  - response_headers (array) - an array modelled after the special $http_response_header variable
+     *  - start_time (float) - the time when the request was sent or 0.0 when it's pending
+     *  - url (string) - the last effective URL of the request
+     *  - user_data (mixed) - the value of the "user_data" request option, null if not set
+     *
+     * When the "capture_peer_cert_chain" option is true, the "peer_certificate_chain"
+     * attribute SHOULD list the peer certificates as an array of OpenSSL X.509 resources.
+     *
+     * Other info SHOULD be named after curl_getinfo()'s associative return value.
+     *
+     * @return mixed An array of all available info, or one of them when $type is
+     *               provided, or null when an unsupported type is requested
+     */
+    public function getHttpInfo(string $type = null): array;
+    
+    /**
      * 
      */
     public function getContent(): ?ParameterBagInterface;
+    
+    public function isSucceed(): bool;
+    
+    public function getMessage(): ?string;
+    
+    public function getErrorNumber(): int;
+    
+    public function getCount(): int;
 
 
 }
